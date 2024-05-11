93a37fee4d08b3c909d954ac3d5087033ff5b344
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-11 09:10:48 -0300

    added null_resource to push to ecr the docker image

--- ecr.tf
+++ ecr.tf
@@ -7,3 +7,19 @@ resource "aws_ecr_repository" "lendaread_ecr" {
   }
 }
 
+resource "null_resource" "docker_image" {
+  depends_on = [aws_ecr_repository.lendaread_ecr]
+
+  provisioner "local-exec" {
+    command = <<EOF
+    aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.lendaread_ecr.repository_url}
+    docker build -t ${aws_ecr_repository.lendaread_ecr.repository_url}:latest .
+    docker push ${aws_ecr_repository.lendaread_ecr.repository_url}:latest
+    EOF
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
