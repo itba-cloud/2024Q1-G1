763bd52bcc3bb18ed95a14233acc35f76c6be002
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-11 10:52:15 -0300

    added load balancer to ECS

---
+++ load_balancer.tf
@@ -0,0 +1,64 @@
+resource "aws_lb" "lendaread_alb" {
+  name               = "lendaread-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.subnet_public1.id, aws_subnet.subnet_public2.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "lendaread-alb"
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.lendaread_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_lb_target_group" "lendaread_tg" {
+  name     = "lendaread-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.lendaread_vpc.id
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.lendaread_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lendaread_tg.arn
+  }
+}
+
