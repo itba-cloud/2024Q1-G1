384cdf21cb361826ddd41456ec800e1f28b2f995
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-11 18:41:24 -0300

    found problem! I had included the nat gateways in the private subnetsgit add . As a result, they were never gonna be able to reach the igw

--- private_subnets.tf
+++ private_subnets.tf
@@ -33,7 +33,7 @@ resource "aws_eip" "nat_eip2" {
 # NAT Gateways
 resource "aws_nat_gateway" "nat_gateway1" {
   allocation_id = aws_eip.nat_eip1.id
-  subnet_id     = aws_subnet.subnet_private1.id
+  subnet_id     = aws_subnet.subnet_public1.id
   tags = {
     Name = "NAT Gateway 1"
   }
@@ -41,7 +41,7 @@ resource "aws_nat_gateway" "nat_gateway1" {
 
 resource "aws_nat_gateway" "nat_gateway2" {
   allocation_id = aws_eip.nat_eip2.id
-  subnet_id     = aws_subnet.subnet_private2.id
+  subnet_id     = aws_subnet.subnet_public2.id
   tags = {
     Name = "NAT Gateway 2"
   }
@@ -52,7 +52,7 @@ resource "aws_route_table" "private_route_table1" {
   vpc_id = aws_vpc.lendaread_vpc.id
 
   route {
-    cidr_block   = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway1.id
   }
 
@@ -65,7 +65,7 @@ resource "aws_route_table" "private_route_table2" {
   vpc_id = aws_vpc.lendaread_vpc.id
 
   route {
-    cidr_block   = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway2.id
   }
 
