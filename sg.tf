dbc9b789320085b85f2f6a85ac9af571a763a846
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-11 09:01:40 -0300

    added sg and updated details of ecs

--- sg.tf
+++ sg.tf
@@ -1,3 +1,4 @@
+# Security Group for ECS tasks
 resource "aws_security_group" "lendaread_api_task_sg" {
   name        = "lendaread_api_sg"
   description = "Security group for ECS tasks to allow traffic on port 8080"
@@ -21,6 +22,31 @@ resource "aws_security_group" "lendaread_api_task_sg" {
 
   tags = {
     Name = "ECS Task Security Group"
+  }
+}
+
+# Security Group for RDS that only allows traffic from the ECS task security group
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-security-group"
+  description = "Security group for RDS instance to allow access from ECS tasks"
+  vpc_id      = aws_vpc.lendaread_vpc.id
+
+  ingress {
+    from_port       = 5432  # PostgreSQL default port
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lendaread_api_task_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "RDS Security Group"
   }
 }
 
