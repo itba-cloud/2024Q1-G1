be02a7e766c8036afa7f348cbcd5f22158e3b4dc
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-11 16:19:51 -0300

    correcting target group lb

--- ecs.tf
+++ ecs.tf
@@ -58,13 +58,18 @@ resource "aws_ecs_service" "lendaread_service" {
   cluster         = aws_ecs_cluster.lendaread_cluster.id
   task_definition = aws_ecs_task_definition.lendaread_api_task.arn
   desired_count   = 1
-
-  launch_type = "FARGATE"
+  launch_type     = "FARGATE"
 
   network_configuration {
     subnets          = [aws_subnet.subnet_public1.id, aws_subnet.subnet_public2.id]
     security_groups  = [aws_security_group.lendaread_api_task_sg.id]
     assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.lendaread_tg.arn
+    container_name   = "lendaread-container"
+    container_port   = 80
   }
 
   depends_on = [
--- load_balancer.tf
+++ load_balancer.tf
@@ -36,6 +36,7 @@ resource "aws_lb_target_group" "lendaread_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.lendaread_vpc.id
+  target_type = "ip"  
 
   health_check {
     enabled             = true
