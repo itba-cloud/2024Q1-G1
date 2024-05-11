ad36342ca8b24edb8f6a2686adf010df410c8d77
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-11 08:25:41 -0300

    added vpc sketch

---
+++ vpc.tf
@@ -0,0 +1,86 @@
+resource "aws_vpc" "lendaread-vpc-tf" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Lendaread VPC"
+  }
+
+
+resource "aws_internet_gateway" "lendaread-gw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "Lendaread Internet Gateway"
+  }
+}
+
+# Public Subnets
+resource "aws_subnet" "subnet_public1" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet 1"
+  }
+}
+
+resource "aws_subnet" "subnet_public2" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet 2"
+  }
+}
+
+
+# Private Subnets
+resource "aws_subnet" "subnet_private1" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Private Subnet 1"
+  }
+}
+
+resource "aws_subnet" "subnet_private2" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Private Subnet 2"
+  }
+}
+
+# Route Tables
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_gw.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_rta1" {
+  subnet_id      = aws_subnet.my_subnet_public1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_rta2" {
+  subnet_id      = aws_subnet.my_subnet_public2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
