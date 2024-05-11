e750a17d0e3b83d5ea06758d4ed6c78ca3e5e56c
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-11 18:42:45 -0300

    updated public subnets

--- public_subnets.tf
+++ public_subnets.tf
@@ -18,10 +18,9 @@ resource "aws_internet_gateway" "lendaread_gw" {
 
 # Public Subnets
 resource "aws_subnet" "subnet_public1" {
-  vpc_id                  = aws_vpc.lendaread_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+  vpc_id            = aws_vpc.lendaread_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "Public Subnet 1"
@@ -29,10 +28,9 @@ resource "aws_subnet" "subnet_public1" {
 }
 
 resource "aws_subnet" "subnet_public2" {
-  vpc_id                  = aws_vpc.lendaread_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+  vpc_id            = aws_vpc.lendaread_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "Public Subnet 2"
