15d9f02801067686a2d0945804a4af7d276491d0
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-11 09:02:55 -0300

    added sg and updated RDS

--- rds.tf
+++ rds.tf
@@ -1,21 +1,32 @@
-resource "aws_db_instance" "lendaread_db" {
-  identifier        = "mydbinstance"
-  instance_class    = "db.t3.micro"  
-  allocated_storage = 20            
+# DB Subnet Group for RDS
+resource "aws_db_subnet_group" "my_db_subnet_group" {
+  name       = "my-db-subnet-group"
   subnet_ids = [aws_subnet.subnet_private1.id, aws_subnet.subnet_private2.id]
 
-  engine                  = "postgres"
-  engine_version          = "13.3" 
-  username                = "postgres"
-  password                = "132htf#" 
-  db_subnet_group_name    = aws_db_subnet_group.my_db_subnet_group.name
-  vpc_security_group_ids  = [aws_security_group.task_security_group.id]
+  tags = {
+    Name = "My DB Subnet Group"
+  }
+}
 
+
+# RDS Instance
+resource "aws_db_instance" "lendaread_db" {
+  identifier        = "mydbinstance"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+  engine            = "postgres"
+  engine_version    = "13.3"
+  username          = "postgres"
+  password          = "132htf#"  # Consider using AWS Secrets Manager
+  db_subnet_group_name    = aws_db_subnet_group.my_db_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   multi_az                = false
   storage_type            = "gp2"
-  skip_final_snapshot     = true 
+  skip_final_snapshot     = true  # Set to false in production environments
 
   tags = {
     Name = "My PostgreSQL Instance"
   }
 }
+
+
