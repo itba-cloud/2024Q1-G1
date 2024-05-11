6ce4414cead87b34e73b260ad197bbd5cd077f28
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-11 18:11:29 -0300

    added load balancer security group@

--- sg.tf
+++ sg.tf
@@ -4,21 +4,13 @@ resource "aws_security_group" "lendaread_lb_sg" {
   description = "Security group for Load Balancer"
   vpc_id      = aws_vpc.lendaread_vpc.id
 
-  # Inbound rules: Allow HTTP and HTTPS traffic
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Outbound rules: Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -37,7 +29,6 @@ resource "aws_security_group" "lendaread_api_task_sg" {
   description = "Security group for ECS tasks to allow traffic on port 8080"
   vpc_id      = aws_vpc.lendaread_vpc.id
 
-  # Inbound rules
   ingress {
     from_port       = 8080
     to_port         = 8080
@@ -45,7 +36,6 @@ resource "aws_security_group" "lendaread_api_task_sg" {
     security_groups = [aws_security_group.lendaread_lb_sg.id]
   }
 
-  # Outbound rules - Allow all outbound traffic by default
   egress {
     from_port   = 0
     to_port     = 0
