1b0b9b5ca8effcf20ee0227ba1cb8ac4755f514c
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-11 15:00:54 -0300

    added cloudwatch to see logs

--- ecs.tf
+++ ecs.tf
@@ -2,10 +2,14 @@ resource "aws_ecs_cluster" "lendaread_cluster" {
   name = "lendaread_cluster" 
 }
 
+resource "aws_cloudwatch_log_group" "lendaread_log_group" {
+  name = "/ecs/lendaread"
+}
+
 resource "aws_ecs_task_definition" "lendaread_api_task" {
   family                   = "lendaread-tasks"
   network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
+  requires_compatibilities = ["EC2"]
   cpu                      = "1024"  # 1 vCPU
   memory                   = "2048"  # 2 GiB
   execution_role_arn       = data.aws_iam_role.lab_role.arn
@@ -23,33 +27,23 @@ resource "aws_ecs_task_definition" "lendaread_api_task" {
           containerPort = 80
           hostPort      = 80
         }
-      ]
+      ],
       environment = [
         { name = "VITE_APP_BASE_PATH", value = "/webapp" },
         { name = "VITE_APP_BASE_URL", value = "http://${aws_lb.lendaread_alb.dns_name}" },
         { name = "DB_URL_ENV", value = "jdbc:postgresql://${aws_db_instance.lendaread_db.endpoint}/" },
         { name = "DB_USERNAME_ENV", value = "${aws_db_instance.lendaread_db.username}" },
         { name = "DB_PASSWORD_ENV", value = "${aws_db_instance.lendaread_db.password}" }  # Consider using AWS Secrets Manager for this
-      ]
+      ],
+      logConfiguration = {
+        logDriver = "awslogs",
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.lendaread_log_group.name
+          awslogs-region        = "us-east-1"  # Adjust this according to your AWS region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
     }
   ])
 }
 
-resource "aws_ecs_service" "lendaread_service" {
-  name            = "lendaread_service"
-  cluster         = aws_ecs_cluster.lendaread_cluster.id
-  task_definition = aws_ecs_task_definition.lendaread_api_task.arn
-  desired_count   = 1
-
-  launch_type = "FARGATE"
-
-  network_configuration {
-    subnets          = [aws_subnet.subnet_public1.id, aws_subnet.subnet_public2.id]
-    security_groups  = [aws_security_group.lendaread_api_task_sg.id]
-  }
-
-  depends_on = [
-    aws_ecr_repository.lendaread_ecr,
-    aws_ecs_task_definition.lendaread_api_task
-  ]
-}
