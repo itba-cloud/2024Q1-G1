dd212d0264db46f061d58ca3f3187e99395881e7
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-12 16:01:48 -0300

    added alb module

---
+++ modules/alb/output.tf
@@ -0,0 +1,15 @@
+output "alb_arn" {
+  description = "The ARN of the Load Balancer."
+  value       = aws_lb.lendaread_alb.arn
+}
+
+output "alb_sg_id" {
+  description = "The ID of the security group attached to the Load Balancer."
+  value       = aws_security_group.alb_sg.id
+}
+
+output "target_group_arn" {
+  description = "The ARN of the target group."
+  value       = aws_lb_target_group.lendaread_tg.arn
+}
+
